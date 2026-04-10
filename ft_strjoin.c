96acf4ffcfc8c2f15100eff725a74fb767964975
gorkgall <gallardo.ahimsa@gmail.com>
2026-04-10 13:06:59 +0200

    ft_strjoin refactorizado y mejorado

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gorkgall <gorkgall@42barcelona.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 12:31:28 by gorkgall          #+#    #+#             */
-/*   Updated: 2026/04/10 12:57:29 by gorkgall         ###   ########.fr       */
+/*   Updated: 2026/04/10 13:06:28 by gorkgall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,19 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	size_t	i;
 	char	*res;
 
-	i = 0;
+	i = -1;
+	if (!s1 || !s2)
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	res = reserver(s1_len, s2_len);
-	if (!res || !s1 || !s2)
+	if (!res)
 		return (NULL);
-	while (s1[i])
-	{
+	while (s1[++i])
 		res[i] = s1[i];
-		i++;
-	}
-	i = 0;
+	i = -1;
 	while (s2[i])
-	{
 		res[s1_len + i] = s2[i];
-		i++;
-	}
 	res[s1_len + i] = '\0';
 	return (res);
 }
