09a76c3db770f66f4fd16d05e55398f22d0f1ad7
gorkgall <gallardo.ahimsa@gmail.com>
2026-04-11 17:10:15 +0200

    libft.h sintax error fixed v3

--- libft.h
+++ libft.h
@@ -34,7 +34,7 @@ char	*ft_strtrim(const char *s1, const char *set);
 char	**ft_split(const char *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-void	*ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 int		ft_atoi(const char *nptr);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
