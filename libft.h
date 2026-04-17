619a40a90a73a941ad76cd7bc629069d578bb380
Gorka Gallardo Castany <gorkgall@cbr11s1.42barcelona.com>
2026-04-17 15:48:04 +0200

    corregida forma y estructura de libft.h, funciones incorporadas al Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gorkgall <gorkgall@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 17:54:52 by gorkgall          #+#    #+#             */
-/*   Updated: 2026/04/17 14:35:00 by gorkgall         ###   ########.fr       */
+/*   Updated: 2026/04/17 15:40:15 by gorkgall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -29,6 +29,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t num, size_t size);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **list, t_list *new);
+void	ft_lstclear(t_list	**lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void));
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(char *big, char *small, size_t n);
@@ -39,7 +45,6 @@ char	*ft_strtrim(const char *s1, const char *set);
 char	**ft_split(const char *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 int		ft_atoi(const char *nptr);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -50,7 +55,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
-
+int		ft_lstsize(t_list *lst);
 typedef struct s_list
 {
 	void			*content;
@@ -58,12 +63,6 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list	*lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void));
-void	ft_lstclear(t_list	**lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(f)(void *), void (*del)(void *));
 #endif
